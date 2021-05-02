d2e8aaee33b93e1085bac762f1a9599ef4775ab8
Daniela Olarte <danielaolarte123@gmail.com>
2021-05-02 18:48:23 -0500

    serialization

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,10 @@
 package model;
 
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1;
 	private char player;
 	private int order;
 	private int moves;
